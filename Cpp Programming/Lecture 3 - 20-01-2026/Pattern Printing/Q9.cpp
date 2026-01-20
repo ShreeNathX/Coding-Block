21fd26f6cba61e648399fb10e18501f96a6781a7
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-01-20 14:32:35 +0530

    Learning C++

--- Cpp Programming/Lecture 3 - 20-01-2026/Pattern Printing/Q9.cpp
+++ Cpp Programming/Lecture 3 - 20-01-2026/Pattern Printing/Q9.cpp
@@ -13,7 +13,7 @@ int main(){
     int n = 5;
     for(int i=1; i<=n; i++){
       for(int j=1; j<=n-i; j++){
-      cout<<" ";
+        cout<<" ";
       }
       for(int k=1; k<=2*i-1; k++){
         cout<<"*";
