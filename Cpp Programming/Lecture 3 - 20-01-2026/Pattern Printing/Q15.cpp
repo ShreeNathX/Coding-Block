c706690f9256b0a036b46a302acb54fed961573d
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-01-22 11:54:17 +0530

    Learning C++

--- Cpp Programming/Lecture 3 - 20-01-2026/Pattern Printing/Q15.cpp
+++ Cpp Programming/Lecture 3 - 20-01-2026/Pattern Printing/Q15.cpp
@@ -3,14 +3,18 @@
     * * * *  
         * * *  
             * *  
-                * 
+                *  
+            * *  
+        * * *  
+    * * * *  
+* * * * *
 */
 
 #include<iostream>
 using namespace std;
 
 int main(){
-    int n = 5;
+    int n=5;
     for(int i=0; i<n; i++){
         for(int j=0; j<2*i; j++){
             cout<<" ";
