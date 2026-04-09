d6c1d56956168fcbe3ca9db98c19acee3dae76eb
Shree Nath Mahato <shreenath.ventures17@gmail.com>
2026-04-10 02:07:48 +0530

    Learning Pattern Printing

--- Cpp Programming/Lecture 4 - 20-01-2026/Pattern Printing/Q17.cpp
+++ Cpp Programming/Lecture 4 - 20-01-2026/Pattern Printing/Q17.cpp
@@ -8,20 +8,55 @@
 * * *   * * *
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n=7;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            if(i==4|| j==4){
-                 cout<<"  ";
-            }else{
-                cout<<"* ";
-            }  
+int main() {
+    int n = 3;
+
+    // Upper part
+    for (int i = 0; i < n; i++) {
+
+        // Left stars
+        for (int j = 0; j < n - i; j++) {
+            cout << "* ";
         }
-        cout<<endl;
+
+        // Spaces
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << "  ";
+        }
+
+        // Right stars
+        for (int j = 0; j < n - i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
     }
 
+    cout << endl;
+
+    // Lower part
+    for (int i = 0; i < n; i++) {
+
+        // Left stars
+        for (int j = 0; j < i + 1; j++) {
+            cout << "* ";
+        }
+
+        // Spaces
+        for (int j = 0; j < 2 * (n - i - 1) + 1; j++) {
+            cout << "  ";
+        }
+
+        // Right stars
+        for (int j = 0; j < i + 1; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
 }
